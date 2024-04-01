7cdf6e44dccaafbeddb409b44cb974b20c514415
Mehrnaz(Marie) <88438830+mehrnaz-MNH@users.noreply.github.com>
2024-04-01 06:05:44 +0000

    manually parsed the json that I was getting !

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -17,19 +17,27 @@ router.get('/api/cheeses', (req, res, next) => {
 // clean after every closing the tab
 const purchases: any[] = []; 
 
-router.post('/api/purchases',(req, res, next) => {
-    try {
-        purchases.push(...req.body);
-        // for debugging purpose
-        console.log("Data received: ", purchases);
-        
-        res.status(200).send('Purchase saved successfully');
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Error occurred while saving purchase');
-    }
-});
+router.post('/api/purchases', (req, res, next) => {
+    let data = '';
 
+    req.on('data', chunk => {
+        data += chunk.toString();
+    });
+
+    req.on('end', () => {
+        try {
+            const jsonData = JSON.parse(data);
+            purchases.push(...jsonData);
+            
+            console.log("Data received: ", purchases);
+            
+            res.status(200).send('Purchase saved successfully');
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).send('Error occurred while saving purchase');
+        }
+    });
+});
 
 
 export default router;
